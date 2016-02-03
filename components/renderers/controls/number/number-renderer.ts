114f582b1e338ada6aaa3970bd78b9dcda846117
Eugen Neufeld <eneufeld@eclipsesource.com>
2016-02-03 11:17:16 +0100

    updated to jsonforms_0.0.15 and fixed enum rendering issue

--- components/renderers/controls/number/number-renderer.ts
+++ components/renderers/controls/number/number-renderer.ts
@@ -4,7 +4,7 @@ class MaterialNumberRenderer implements JSONForms.IRenderer {
 
     priority = 10;
 
-    render(element: IUISchemaElement, subSchema: SchemaElement, schemaPath: string, services: JSONForms.Services) {
+    render(element: IControlObject, subSchema: SchemaElement, schemaPath: string, services: JSONForms.Services) {
         var control = JSONForms.RenderDescriptionFactory.createControlDescription(schemaPath, services, element);
         control['template'] = `<jsonforms-material-control><input type="number" step="0.01" id="${schemaPath}" aria-label="{{element.label}}" data-jsonforms-validation data-jsonforms-model/></jsonforms-material-control>`;
         return control;
